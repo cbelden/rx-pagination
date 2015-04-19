791bb714d9df247b282e17a03f8f32a1a4438706
Calvin Belden <calvinbelden@gmail.com>
2015-04-19 12:02:40 -0500

    Created styles folder for .css. Moved movie data to its own file. Fixed bug where queries with spaces caused false positive match highlighting.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var concat = require('gulp-concat');
 
 gulp.task('bundle', function () {
-    var bundleStream = browserify('./rx-pagination.js').bundle();
+    var bundleStream = browserify('./rx-pagination.js', {debug: true}).bundle();
 
     bundleStream
         .pipe(source('./rx-pagination.js'))
         .pipe(gulp.dest('./bin'));
 });
+
+gulp.task('watch', function () {
+    gulp.watch('./rx-pagination.js', ['bundle']);
+})
