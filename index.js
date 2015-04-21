49d3b9ba2b2deb3f14396a59030c75800952b3f3
Calvin Belden <calvinbelden@gmail.com>
2015-04-20 21:15:57 -0500

    Removed unused method. Added more verbose comments.

--- index.js
+++ index.js
@@ -21,7 +21,9 @@
 
 
 /**
- *  Filter updates (ie when a user enters new text in the search box)
+ *  Filter updates. Each time a user enters text in the search box, we will
+ *  map this event to a string value (the entered text) and push it to our
+ *  data stream.
  */
 var searchInput = document.getElementById('search');
 var searchEvents = Rx.Observable.fromEvent(searchInput, 'keyup')
@@ -31,7 +33,10 @@ var searchEvents = Rx.Observable.fromEvent(searchInput, 'keyup')
 
 
 /**
- *  Paging updates (ie when a user selects a new page). This pattern was found at
+ *  Paging updates (ie when a user selects a new page). Every time an <a> element
+ *  is clicked, we will map that event to a number and push it to our data stream.
+ *
+ *  This pattern was found at
  *  https://github.com/Reactive-Extensions/RxJS/blob/master/doc/gettingstarted/events.md to
  *  subsribe to events for dom elements that may/may not be present yet.
  */
@@ -45,7 +50,10 @@ var pagingEvents = Rx.Observable.fromEventPattern(
     .startWith(1);
 
 /**
- *  Combination of filter and paging updates (will capture any filtering/paging changes)
+ *  Here we combine the search events and the paging events. We can think of the
+ *  search events as the parent stream, which then spawns other streams when
+ *  different pages get selected. I think this is easier to conceptulaize by
+ *  playing with the demo.
  */
 var pageUpdates = searchEvents
     .selectMany(function (search) {
@@ -62,17 +70,25 @@ var pageUpdates = searchEvents
 
 
 /**
- *  Stream of page models (created when a new page update occurs)
+ *  Stream of page models (created when a new page update occurs). We will map
+ *  each user input to a new page model, which represents the current state of
+ *  the application/page. We will later subscribe to this stream to update the UI.
  */
 var pageModels = pageUpdates
     .map(function (options) {
+        // Filter our results based on the supplied filter (mapped from text-input)
         var results = JSON.parse(JSON.stringify(resultsStore))
             .filter(function (result) {
                 return options.filter(result.title);
             });
 
-        var paginatedResults = paginate(Rx.Observable.from(results), options);
+        // Apply pagination
+        var paginatedResults = Rx.Observable.from(results)
+            .skip((options.page - 1) * options.pageSize)
+            .take(options.pageSize);;
 
+        // Create a stream of page numbers (displayed at the bottom of the list for
+        // navigation)
         var numberOfPages = Math.ceil(results.length / options.pageSize);
         var pageNumbers = Rx.Observable
             .range(1, numberOfPages)
@@ -88,7 +104,7 @@ var pageModels = pageUpdates
 
 
 /**
- * Subscribe to the model updates and re-render the page
+ * Here we subscribe to the stream of model updates and rerender the page.
  */
 pageModels.subscribe(function render (model) {
     model.results
@@ -111,24 +127,17 @@ pageModels.subscribe(function render (model) {
 
 
 /**
- * Pagination and Search
+ * Search
  */
 
 
-function paginate (results, options) {
-    return results
-            .skip((options.page - 1) * options.pageSize)
-            .take(options.pageSize);
-}
-
-
 function search (query) {
+
+    /*
+     * Very rudimentary search filter.
+     */
     return function (target) {
         if (!query) return true;
-
-        /*
-         * Very rudimentary search filter.
-         */
         return target.toLowerCase().indexOf(query.toLowerCase()) !== -1;
     };
 }
